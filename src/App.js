6d1e59b811aec7da9adb80d8b7bd952bbc70950c
Hanh Nga <54220859+unagiholix@users.noreply.github.com>
2021-10-04 12:04:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+
+class Form extends React.Component {}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
