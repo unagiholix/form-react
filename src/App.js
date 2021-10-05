c1f75f8866b27a69a53b94731435d947aac6bb77
Hanh Nga <54220859+unagiholix@users.noreply.github.com>
2021-10-06 01:32:49 +0200

    Having trouble to re-create React Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./styles.css";
 import React from "react";
 
-class Form extends React.Component {}
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    this.emailRef = React.createRef();
+    this.passwordRef = React.createRef();
+  }
+
+  handleClick(e) {
+    alert("User " + this.emailRef.current.value + "has logged in");
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          {" "}
+          Email
+          <input value={this.emailRef} />
+        </label>
+        <br />
+        <label>
+          {" "}
+          Password
+          <input value={this.passwordRef} />
+          <button type="submit" onClick={this.handleClick}>
+            Submit
+          </button>
+        </label>
+      </form>
+    );
+  }
 }
+
+export default Form;
